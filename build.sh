a4b24cf6dde418d0859389508a9ec38d22a52f62
Dmitry Pranchuk <d.pranchuk@gmail.com>
2017-01-09 12:58:02 +0300

    remove temp files as traps

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+trap 'rm ca-certificates.crt' EXIT
+trap 'rm main' EXIT
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
 curl -o ca-certificates.crt https://curl.haxx.se/ca/cacert.pem
 docker build -t cortwave/spark-monitor:$1 .
-rm ca-certificates.crt
-rm main
